50d0a89c7dfeb7ef37979ac46c37c6f01e662772
Marcos Paulo <mpcc.show@gmail.com>
2021-11-11 15:13:12 -0300

    Implementação inicial do TransacaoRepository

---
+++ project/src/main/java/com/bluebank/project/repositories/TransacaoRepository.java
@@ -0,0 +1,11 @@
+package com.bluebank.project.repositories;
+
+import com.bluebank.project.models.Transacao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
+  
+}
