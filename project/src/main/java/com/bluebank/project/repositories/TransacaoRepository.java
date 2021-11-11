870b6215cc957aea3d1003611d993d9685cac8a4
Marcos Paulo <mpcc.show@gmail.com>
2021-11-11 18:01:01 -0300

    Implementação do TransacaoService

--- project/src/main/java/com/bluebank/project/repositories/TransacaoRepository.java
+++ project/src/main/java/com/bluebank/project/repositories/TransacaoRepository.java
@@ -1,5 +1,6 @@
 package com.bluebank.project.repositories;
 
+import com.bluebank.project.models.Conta;
 import com.bluebank.project.models.Transacao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
   
+  public Transacao findByCpfCnpj(String cpfcnpj);
+
+  public Transacao criarTransferencia(Conta contaDestino);
+
 }
