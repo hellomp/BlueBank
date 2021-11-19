072124c67852255d9061263a4df6a17354ebd9ae
Pedro Henrique <pedrohbc96@gmail.com>
2021-11-18 21:39:22 -0300

    atualiza mapper de transacoes

--- project/src/main/java/com/bluebank/project/mappers/TransacaoMapper.java
+++ project/src/main/java/com/bluebank/project/mappers/TransacaoMapper.java
@@ -1,30 +1,90 @@
 package com.bluebank.project.mappers;
 
-import org.mapstruct.MappingTarget;
+
+
+import org.springframework.stereotype.Service;
 
 import com.bluebank.project.dtos.DepositoDTO;
 import com.bluebank.project.dtos.EmprestimoDTO;
 import com.bluebank.project.dtos.SaqueDTO;
 import com.bluebank.project.dtos.TransacaoDTO;
 import com.bluebank.project.dtos.TransferenciaDTO;
+import com.bluebank.project.models.Emprestimo;
 import com.bluebank.project.models.Transacao;
 
-//@Mapper(componentModel = "spring")
-public interface TransacaoMapper {
+@Service
+public class TransacaoMapper {
+
+//	public Transacao updateTransacaoFromDto(TransacaoDTO transacaoDTO, Transacao transacao) {
+//	}
+	public TransacaoDTO updateDtoFromTransacao(Transacao transacao, TransacaoDTO transacaoDTO) {
+		transacaoDTO.setConta(transacao.getConta());
+		transacaoDTO.setTipoTransacao(transacao.getTipoTransacao());
+		transacaoDTO.setDataTransacao(transacao.getDataTransacao());
+		transacaoDTO.setSaldoAnterior(transacao.getSaldoAnterior());
+		transacaoDTO.setSaldoAtual(transacao.getSaldoAtual());
+		transacaoDTO.setContaDestino(transacao.getContaDestino());
+		transacaoDTO.setDataAgendTransacao(transacao.getDataAgendTransacao());
+		transacaoDTO.setDataExecTransacao(transacao.getDataExecTransacao());
+		transacaoDTO.setEmprestimo(transacao.getEmprestimo());
+		transacaoDTO.setDepositoEmprestimo(transacao.getDepositoEmprestimo());
+		return transacaoDTO;
+		
+	}
 	
-//	@BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-	void updateTransacaoFromDto(TransacaoDTO transacaoDTO, @MappingTarget Transacao transacao);
-	void updateDtoFromTransacao(Transacao transacao, @MappingTarget TransacaoDTO transacaoDTO);
+//	public Transacao updateTransacaoFromDepositoDto(DepositoDTO depositoDTO, Transacao transacao) {
+//	}	
+	public DepositoDTO updateDepositoDtoFromTransacao(Transacao transacao,DepositoDTO depositoDTO) {
+		depositoDTO.setConta(transacao.getConta());
+		depositoDTO.setDataTransacao(transacao.getDataTransacao());
+		depositoDTO.setTipoTransacao(transacao.getTipoTransacao());
+		depositoDTO.setSaldoAnterior(transacao.getSaldoAnterior());
+		depositoDTO.setSaldoAtual(transacao.getSaldoAtual());
+		return depositoDTO;
+	}
+
 	
-	void updateTransacaoFromDepositoDto(DepositoDTO depositoDTO, @MappingTarget Transacao transacao);
-	void updateDepositoDtoFromTransacao(Transacao transacao, @MappingTarget DepositoDTO depositoDTO);
+//	public Emprestimo updateEmprestimoFromEmprestimoDto(EmprestimoDTO emprestimoDTO, Emprestimo emprestimo) {	
+//	}
+	public EmprestimoDTO updateEmprestimoDtoFromEmprestimo(Emprestimo emprestimo, EmprestimoDTO emprestimoDTO) {
+		emprestimoDTO.setNumeroContrato(emprestimo.getNumeroContrato());
+		emprestimoDTO.setDataInicio(emprestimo.getDataInicio());
+		emprestimoDTO.setDataFim(emprestimo.getDataFim());
+		emprestimoDTO.setValorEmprestimo(emprestimo.getValorEmprestimo());
+		emprestimoDTO.setPercentualJuros(emprestimo.getPercentualJuros());
+		emprestimoDTO.setQuantParcelas(emprestimo.getQuantParcelas());
+		return emprestimoDTO;
+	}
 	
-	void updateTransacaoFromEmprestimoDto(EmprestimoDTO emprestimoDTO, @MappingTarget Transacao transacao);
-	void updateEmprestimoDtoFromTransacao(Transacao transacao, @MappingTarget EmprestimoDTO emprestimoDTO);
 	
-	void updateTransacaoFromSaqueDto(SaqueDTO saqueDTO, @MappingTarget Transacao transacao);
-	void updateSaqueDtoFromTransacao(Transacao transacao, @MappingTarget SaqueDTO saqueDTO);
 	
-	void updateTransacaoFromTransferenciaDto(TransferenciaDTO transferenciaDTO, @MappingTarget Transacao transacao);
-	void updateTransferenciaDtoFromTransacao(Transacao transacao, @MappingTarget TransferenciaDTO transferenciaDTO);
+//	public Transacao updateTransacaoFromSaqueDto(SaqueDTO saqueDTO,Transacao transacao){
+//}
+	public SaqueDTO updateSaqueDtoFromTransacao(Transacao transacao, SaqueDTO saqueDTO) {
+		saqueDTO.setConta(transacao.getConta());
+		saqueDTO.setTipoTransacao(transacao.getTipoTransacao());
+		saqueDTO.setDataTransacao(transacao.getDataTransacao());
+		saqueDTO.setSaldoAnterior(transacao.getSaldoAnterior());
+		saqueDTO.setSaldoAtual(transacao.getSaldoAtual());
+		return saqueDTO;
+	}
+
+//	public Transacao updateTransacaoFromTransferenciaDto(TransferenciaDTO transferenciaDTO, @MappingTarget Transacao transacao){
+//}
+	public TransferenciaDTO updateTransferenciaDtoFromTransacao(Transacao transacao, TransferenciaDTO transferenciaDTO) {
+		transferenciaDTO.setConta(transacao.getConta());
+		transferenciaDTO.setTipoTransacao(transacao.getTipoTransacao());
+		transferenciaDTO.setDataTransacao(transacao.getDataTransacao());
+		transferenciaDTO.setSaldoAnterior(transacao.getSaldoAnterior());
+		transferenciaDTO.setSaldoAtual(transacao.getSaldoAtual());
+		transferenciaDTO.setContaDestino(transacao.getContaDestino());
+		transferenciaDTO.setDataAgendTransacao(transacao.getDataAgendTransacao());
+		transferenciaDTO.setDataExecTransacao(transacao.getDataExecTransacao());
+		return transferenciaDTO;
+	}
+
 }
+
+
+
+
