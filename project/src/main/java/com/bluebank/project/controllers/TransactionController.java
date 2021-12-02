e34a7e8af21dc03bdc91ded20ea53c1b719db2c8
Hellimateas Barros <hellimateasbarros@gmail.com>
2021-12-02 13:28:48 -0300

    finalizado documentação Swagger

--- project/src/main/java/com/bluebank/project/controllers/TransactionController.java
+++ project/src/main/java/com/bluebank/project/controllers/TransactionController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,26 +19,34 @@ import com.bluebank.project.dtos.WithdrawDTO;
 import com.bluebank.project.models.Transaction;
 import com.bluebank.project.services.TransactionService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/transacao")
+@Api(value="API REST transação")
+@CrossOrigin(origins="*")
 public class TransactionController {
   
   @Autowired
   TransactionService transacaoService;
 
   @PostMapping("/saque/{id}")
+  @ApiOperation(value="Este método faz um saque atraves de uma transação")
 	@ResponseStatus(HttpStatus.CREATED)
 	public WithdrawDTO withdraw(@PathVariable("id") Long id, @RequestBody Transaction transacao) {
 		return transacaoService.withdrawAmount(id, transacao);
 	}
 	
 	@PostMapping("/deposito/{id}")
+	@ApiOperation(value="Este método faz um depósito atraves de uma transação")
 	@ResponseStatus(HttpStatus.CREATED)
 	public DepositDTO deposit(@PathVariable("id") Long id, @RequestBody Transaction transacao) {
 		return transacaoService.depositAmount(id, transacao);
 	}
 	
 	@GetMapping("/saldo/{id}")
+	@ApiOperation(value="Este método consulta o saldo do cliente")
 	@ResponseStatus(HttpStatus.OK)
 	public double balance(@PathVariable("id") Long id){
 		return transacaoService.showAccountBalanceById(id);
