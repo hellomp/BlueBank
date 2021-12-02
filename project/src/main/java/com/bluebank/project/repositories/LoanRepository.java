9414e2b6ee06c4cf4a73898d633f9dfe5c3bdb0c
icaro pablo <ic4ro.p4blo@gmail.com>
2021-12-02 13:19:57 -0300

    adicionadas as primeiras descriçoes dos métodos usando javadoc todas as interfaces e métodos do pacote de repositories foram descritos

--- project/src/main/java/com/bluebank/project/repositories/LoanRepository.java
+++ project/src/main/java/com/bluebank/project/repositories/LoanRepository.java
@@ -7,8 +7,20 @@ import com.bluebank.project.models.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+* Interface for creating queries to the loans table
+* @author Marcos Paulo
+*/
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long>{
 
-  public List<Loan> findByClientId_Cpfcnpj(String cpfcnpj);
+	/**
+	* Returns the loan to which the given cpf or cnpj matches its account's client
+	* containing all the corresponding database table's columns data into its attributes
+	* 
+	* @param  cpfcnpj  a string formed just by numbers of the client entity's cpf or cnpj attribute
+	* @return a loan object to which the cpf or cnpj matches the search
+	*/
+	public List<Loan> findByClientId_Cpfcnpj(String cpfcnpj);
+  
 }
